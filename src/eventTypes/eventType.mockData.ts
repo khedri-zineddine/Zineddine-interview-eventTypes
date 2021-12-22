c3c8c9f60d00c35089f842865da35c49be419683
AliBenCafu <th3gooner@gmail.com>
2021-12-22 13:17:00 +0300

    Add Mock Data

--- src/eventTypes/eventType.mockData.ts
+++ src/eventTypes/eventType.mockData.ts
@@ -1,4 +1,16 @@
 export const eventTypes = [
-  { _id: 'guidx', id: 1, name: '', description: '', eventType: '' },
-  { _id: 'guidy', id: 9999, name: '', description: '', eventType: '' },
+  {
+    id: '920bc069-5390-476f-bb2d-4d9d898bb7e1',
+    externalId: 1,
+    name: '',
+    description: '',
+    eventType: '',
+  },
+  {
+    id: '9452962b-e13f-48a3-8f95-8ba4ec76e922',
+    externalId: 9999,
+    name: '',
+    description: '',
+    eventType: '',
+  },
 ];
